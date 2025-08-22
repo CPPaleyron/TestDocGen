378bb6ed3d8082930e12b7ef2d67b054cf331294
CPPaleyron <CPPaleyron@users.noreply.github.com>
2025-08-22 10:41:56 +0000

    deploy: 7c5fbc2c805efbf8b1e6972536115b39650c3afc

--- navtreedata.js
+++ navtreedata.js
@@ -48,5 +48,6 @@ var NAVTREEINDEX =
 "Program_8cs.html"
 ];
 
-var SYNCONMSG = 'click to disable panel synchronisation';
-var SYNCOFFMSG = 'click to enable panel synchronisation';
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
+var LISTOFALLMEMBERS = 'List of all members';
